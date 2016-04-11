c415d484b2b7710a4aa714582d2908a170bd51f2
Suflow <suflow.com@gmail.com>
2016-04-11 08:08:17 +0200

    Update DatabaseService.cs

--- Service/DatabaseService.cs
+++ Service/DatabaseService.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +17,16 @@ namespace Suflow.Common.Utils {
         /// NOTE: You can use serialize/deserialize of
         ///     https://github.com/Suflow/Suflow.Common.Utils/blob/master/Extension/ObjectExtensions.cs
         /// </summary>
-        public DatabaseServer Import(string content) {
+        public DatabaseServer ImportFromString(string content) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This method receives database Server information (server name, user, password and databases)
+        /// And connects to the server and create corresponding database server object out of it.
+        /// databases is empty, it will consider all databases
+        /// </summary>
+        public DatabaseServer ImportFromServer(string databaseServer, string user, string password, string[] databases) {
             throw new NotImplementedException();
         }
 
@@ -27,15 +36,29 @@ namespace Suflow.Common.Utils {
         /// NOTE: You can use serialize/deserialize of
         ///     https://github.com/Suflow/Suflow.Common.Utils/blob/master/Extension/ObjectExtensions.cs
         /// </summary>
-        public string Export(DatabaseServer server) {
+        public string ExportToString(DatabaseServer server) {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// This method receives from and to 
+        /// This method receives from and to and generate upgrade script in SQL
         /// </summary>
         public string GenerateUpgradeScript(DatabaseServer from, DatabaseServer to) {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ExecuteScript(string connectionString, string upgradeScript) {
+        }
+
+        /// <summary>
+        /// This method receives database server object and export it to real server
+        /// 
+        /// </summary>
+        /// <param name="server"></param>
+        public void UpgradeServer(DatabaseServer server) {
         }
     }
 }
