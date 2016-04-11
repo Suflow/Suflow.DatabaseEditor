62b9b1f2df47562b31788cbb93b2a41c5432fe1a
Suflow <suflow.com@gmail.com>
2016-04-11 07:42:43 +0200

    Starting...

---
+++ DatabaseService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suflow.Common.Utils {
+    /// <summary>
+    /// Feel free to refactor (add/remove/rename) as per need!
+    /// Create unit test to test each service method
+    /// </summary>
+    public class DatabaseService {
+        /// <summary>
+        /// This method receives string representation of database server and convert it to
+        /// database server object.
+        /// 
+        /// NOTE: You can use serialize/deserialize of
+        ///     https://github.com/Suflow/Suflow.Common.Utils/blob/master/Extension/ObjectExtensions.cs
+        /// </summary>
+        public DatabaseServer Import(string content) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This method receives DatabaseServer and convert it to string
+        /// 
+        /// NOTE: You can use serialize/deserialize of
+        ///     https://github.com/Suflow/Suflow.Common.Utils/blob/master/Extension/ObjectExtensions.cs
+        /// </summary>
+        public string Export(DatabaseServer server) {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// This method receives from and to 
+        /// </summary>
+        public string GenerateUpgradeScript(DatabaseServer from, DatabaseServer to) {
+            throw new NotImplementedException();
+        }
+    }
+}
